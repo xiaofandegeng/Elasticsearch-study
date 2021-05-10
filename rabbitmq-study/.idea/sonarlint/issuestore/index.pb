41060af8aec4ded5e575d0894c941cc0d259243b
liaohongwei <434962239@qq.com>
2021-05-10 18:07:06 +0800

    新增内容

---
+++ rabbitmq-study/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,23 @@
+
+N
+springboot-consumer/.gitignore,8\7\874abe9d9fe7b45a83c365305c7469aad2f2a57a
+K
+springboot-producer/pom.xml,c\4\c4405f2bdd16d79b841b2cc73277d71f5687ba9b
+K
+springboot-consumer/pom.xml,c\6\c68675e165f3eff7a7129e6bc2041e47fc43f7f2
+z
+Jspringboot-consumer/src/test/java/com/itcast/ConsumerApplicationTests.java,7\b\7b54898298c8377ff4ca3badf4b7f09429589f04
+€
+Pspringboot-producer/src/main/java/com/itcast/rabbitmq/config/RabbitmqConfig.java,4\e\4ea8e9d851fabb64db7f5f43d1aa03b3efe370b3
+|
+Lspringboot-producer/src/test/java/com/itcast/rabbitmq/test/ProducerTest.java,7\3\734d6e529ef75664c19ecf4c525c0c49c2e09f1f
+{
+Kspringboot-consumer/src/main/java/com/itcast/listener/RabbitMQListener.java,4\a\4acca1b8f413622d15c06787061e670132929f04
+u
+Espringboot-consumer/src/main/java/com/itcast/ConsumerApplication.java,9\9\99fa9c6a6a121b9c9eb85d2883fa091378010f2b
+P
+ spring-rabbitmq-producer/pom.xml,3\e\3ebb71479d602270f2c38d8d66dbdee6443fb04c
+P
+ spring-rabbitmq-consumer/pom.xml,f\4\f4ec6d9eb547b21ba94c5678ebce66bd231e9490
+{
+Kspring-rabbitmq-producer/src/test/java/com/itcast/rabbitmq/test/Test01.java,4\c\4cf6da1533b108c3044b798f52fcc240fa8974c0
--- rabbitmq-study/spring-rabbitmq-producer/src/test/java/com/itcast/rabbitmq/test/Test01.java
+++ rabbitmq-study/spring-rabbitmq-producer/src/test/java/com/itcast/rabbitmq/test/Test01.java
@@ -2,6 +2,8 @@ package com.itcast.rabbitmq.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -41,5 +43,43 @@ public class Test01 {
     public void testTopic() {
         //发送消息
         rabbitTemplate.convertAndSend("spring_topic_exchange", "heima.log.hah", "hello,spring topic......");
+    }
+
+    // 测试确认消息
+    @Test
+    public void testConfirm() {
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            @Override
+            public void confirm(CorrelationData correlationData, boolean b, String s) {
+                System.out.println("confirm方法被执行了......");
+                if (b) {
+                    System.out.println("消息接收成功：" + s);
+                }else {
+                    System.out.println("消息接收失败：" + s);
+                }
+            }
+        });
+
+        // 发送消息
+        rabbitTemplate.convertAndSend("test_exchange_confirm111", "confirm","confirm message....");
+    }
+    // 测试回退消息
+    @Test
+    public void testReturn() {
+        rabbitTemplate.setMandatory(true);
+
+        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+            @Override
+            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                System.out.println("return方法被执行了......");
+                System.out.println(message);
+                System.out.println(replyCode);
+                System.out.println(replyText);
+                System.out.println(exchange);
+                System.out.println(routingKey);
+            }
+        });
+
+        rabbitTemplate.convertAndSend("test_exchange_confirm", "return2132", "return message......");
     }
 }
