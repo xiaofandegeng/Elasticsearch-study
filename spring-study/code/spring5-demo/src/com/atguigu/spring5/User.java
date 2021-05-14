6a1112efc63ff747596306e14da7e22ae96e03ee
liaohongwei <434962239@qq.com>
2021-05-14 15:53:45 +0800

    spring5学习

---
+++ spring-study/code/spring5-demo/src/com/atguigu/spring5/User.java
@@ -0,0 +1,14 @@
+package com.atguigu.spring5;
+
+/**
+ * <p>DESC: user用户类</p>
+ * <p>DATE: 2021/5/14</p>
+ * <p>VERSION:1.0.0</p>
+ * <p>@AUTHOR: liaohongwei</p>
+ */
+public class User {
+
+    public void add() {
+        System.out.println("add方法被执行......");
+    }
+}
---
+++ spring-study/code/spring5-demo/src/com/atguigu/spring5/test/DemoTest01.java
@@ -0,0 +1,24 @@
+package com.atguigu.spring5.test;
+
+import com.atguigu.spring5.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * <p>DESC: TODO</p>
+ * <p>DATE: 2021/5/14</p>
+ * <p>VERSION:1.0.0</p>
+ * <p>@AUTHOR: liaohongwei</p>
+ */
+public class DemoTest01 {
+
+    @Test
+    public void testAdd() {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean1.xml");
+        User user = context.getBean("user", User.class);
+        user.add();
+
+    }
+}
