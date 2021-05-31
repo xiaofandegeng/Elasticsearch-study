d7a3f89a36080bcd17ce41b5e8e9830bb51507f1
liaohongwei <434962239@qq.com>
2021-05-31 11:23:17 +0800

    完成逆波兰表达式计算器

--- suanfa-study/src/com/atguigu/stack/PolandNotation.java
+++ suanfa-study/src/com/atguigu/stack/PolandNotation.java
@@ -3,6 +3,7 @@ package com.atguigu.stack;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * <p>DESC: 逆波兰计算器</p>
@@ -27,15 +28,38 @@ public class PolandNotation {
      * @return 计算结果res
      */
     private static int cal(List<String> list) {
-        int res = 0;
+        int res;
+        Stack<String> stack = new Stack<>();
         //  循环这个list，如果是数字放入栈中，如果是计算符取出计算
         for (String el : list) {
             //  el是数字
-            if(el.matches("//d+")){
-                System.out.println(el);
+            if (el.matches("\\d+")) {
+                stack.add(el);
+            }
+            //  el是符号
+            else {
+                int num1 = Integer.parseInt(stack.pop());
+                int num2 = Integer.parseInt(stack.pop());
+                switch (el) {
+                    case "+":
+                        res = num1 + num2;
+                        break;
+                    case "-":
+                        res = num2 - num1;
+                        break;
+                    case "*":
+                        res = num1 * num2;
+                        break;
+                    case "/":
+                        res = num2 / num1;
+                        break;
+                    default:
+                        throw new RuntimeException("传入的运算符有误会，请修改");
+                }
+                stack.push(String.valueOf(res));
             }
         }
-
+        res = Integer.parseInt(stack.pop());
         return res;
     }
 
