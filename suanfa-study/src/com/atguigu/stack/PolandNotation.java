c3dbeff652bb0a0b018195cf0d68539c12729fdc
liaohongwei <434962239@qq.com>
2021-05-31 10:55:59 +0800

    change

---
+++ suanfa-study/src/com/atguigu/stack/PolandNotation.java
@@ -0,0 +1,55 @@
+package com.atguigu.stack;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * <p>DESC: 逆波兰计算器</p>
+ * <p>DATE: 2021/5/31</p>
+ * <p>VERSION:1.0.0</p>
+ * <p>@AUTHOR: lhw</p>
+ */
+public class PolandNotation {
+    public static void main(String[] args) {
+        //  先定义逆波兰表达式 (3+4)*5-6 =>  3 4 + 5 * 6 - (结果是29)
+        String str = "3 4 + 5 * 6 -";
+        List<String> list = getList(str);
+        System.out.println(list);
+        int res = cal(list);
+        System.out.println("计算结果为： " + res);
+    }
+
+    /**
+     * 计算器
+     *
+     * @param list 分割好的list集合
+     * @return 计算结果res
+     */
+    private static int cal(List<String> list) {
+        int res = 0;
+        //  循环这个list，如果是数字放入栈中，如果是计算符取出计算
+        for (String el : list) {
+            //  el是数字
+            if(el.matches("//d+")){
+                System.out.println(el);
+            }
+        }
+
+        return res;
+    }
+
+    /**
+     * 将字符串转化为list集合
+     *
+     * @param str 字符串
+     * @return list集合
+     */
+    public static List<String> getList(String str) {
+        List<String> list = new ArrayList<>();
+        String[] s = str.split(" ");
+        Collections.addAll(list, s);
+        return list;
+    }
+
+}
