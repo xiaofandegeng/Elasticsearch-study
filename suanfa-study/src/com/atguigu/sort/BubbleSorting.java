ad30a1b8196b63778a0d528b04ff552c2dd96aff
liaohongwei <434962239@qq.com>
2021-06-01 16:26:44 +0800

    冒泡排序时间消耗

--- suanfa-study/src/com/atguigu/sort/BubbleSorting.java
+++ suanfa-study/src/com/atguigu/sort/BubbleSorting.java
@@ -1,6 +1,8 @@
 package com.atguigu.sort;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * <p>DESC: 冒泡排序</p>
@@ -10,15 +12,37 @@ import java.util.Arrays;
  */
 public class BubbleSorting {
     public static void main(String[] args) {
-        int[] arr = {-3, -1, 9, 10, -2};
+        int size = 80000;
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = (int) (Math.random() * 800000);
+        }
 
+        Date before = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String beforeDate = simpleDateFormat.format(before);
+        System.out.println("排序前的时间为：" + beforeDate);
+        bubbleSorting(arr);
+
+        Date after = new Date();
+        String afterDate = simpleDateFormat.format(after);
+        System.out.println("排序前的时间为：" + afterDate);
+    }
+
+    /**
+     * 冒泡排序的方法
+     *
+     * @param arr 输入数组
+     */
+    private static void bubbleSorting(int[] arr) {
         //  冒泡排序的优化，添加一个符号，如果这次没有移动，则表示排序已结束
         boolean flag = false;
+        int temp;
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     flag = true;
-                    int temp = arr[j];
+                    temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
@@ -30,7 +54,7 @@ public class BubbleSorting {
                 break;
             }
 
-            System.out.println("第" + (i + 1) + "趟排序后的结果为： " + Arrays.toString(arr));
+//            System.out.println("第" + (i + 1) + "趟排序后的结果为： " + Arrays.toString(arr));
         }
     }
 }
---
+++ suanfa-study/src/com/atguigu/sort/SelectSorting.java
@@ -0,0 +1,13 @@
+package com.atguigu.sort;
+
+/**
+ * <p>DESC: 选择排序算法</p>
+ * <p>DATE: 2021/6/1</p>
+ * <p>VERSION:1.0.0</p>
+ * <p>@AUTHOR: lhw</p>
+ */
+public class SelectSorting {
+    public static void main(String[] args) {
+        int[] arr = {10, 8, 45, 34, 57, 15, 64, 78, 55, 10};
+    }
+}
