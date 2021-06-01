083f5c5e858d98c3bc8fd9435034636b82e53fec
xiaofandegeng <434962239@qq.com>
2021-06-01 20:58:47 +0800

    推导的方式写出冒泡排序

--- suanfa-study/src/com/atguigu/sort/BubbleSorting.java
+++ suanfa-study/src/com/atguigu/sort/BubbleSorting.java
@@ -1,8 +1,6 @@
 package com.atguigu.sort;
 
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 
 /**
  * <p>DESC: 冒泡排序</p>
@@ -12,12 +10,17 @@ import java.util.Date;
  */
 public class BubbleSorting {
     public static void main(String[] args) {
-        int[] arr = CommonMethods.getArr(80000);
+//        int[] arr = CommonMethods.getArr(80000);
+//
+//        long before = CommonMethods.getTime();
+//        bubbleSorting(arr);
+//        long after = CommonMethods.getTime();
+//        System.out.println("排序总共消费：" + (after - before) + " 时间");
 
-        long before = CommonMethods.getTime();
-        bubbleSorting(arr);
-        long after = CommonMethods.getTime();
-        System.out.println("排序总共消费：" + (after - before) + " 时间");
+        int[] deriveArr = {13, 11, 15, 9, 10};
+        derive(deriveArr);
+
+
     }
 
     /**
@@ -48,4 +51,77 @@ public class BubbleSorting {
 //            System.out.println("第" + (i + 1) + "趟排序后的结果为： " + Arrays.toString(arr));
         }
     }
+
+
+    /**
+     * 冒泡排序的推导过程
+     *
+     * @param arr 需要进行排序的数组
+     * @return 排序后的数组
+     */
+    public static void derive(int[] arr) {
+        // 第一次排序
+        int temp;
+        for (int j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        System.out.println("第一次排序后的结果：" + Arrays.toString(arr));
+
+        // 第二次排序
+        for (int j = 0; j < arr.length - 1 - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        System.out.println("第二次排序后的结果：" + Arrays.toString(arr));
+
+        // 第三次排序
+        for (int j = 0; j < arr.length - 1 - 2; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        System.out.println("第三次排序后的结果：" + Arrays.toString(arr));
+
+        // 第四次排序
+        for (int j = 0; j < arr.length - 1 - 3; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        System.out.println("第四次排序后的结果：" + Arrays.toString(arr));
+
+        boolean flag = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            // 第四次排序
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    flag = true;
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            // 如果flag为真，则表示进入了循环的，则下一次可能要排序，否则则不再需要排序了
+            if(flag){
+                flag = false;
+            }else {
+                break;
+            }
+            System.out.println("第"+i+"次排序后的结果：" + Arrays.toString(arr));
+        }
+
+    }
+
+
 }
