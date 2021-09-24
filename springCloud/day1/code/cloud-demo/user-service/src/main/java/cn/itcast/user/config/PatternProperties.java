9bb21cccdd80c566541d38d118064fae560ce706
xiaofandegeng <434962239@qq.com>
2021-09-24 16:09:05 +0800

    从nacos读取配置

---
+++ springCloud/day1/code/cloud-demo/user-service/src/main/java/cn/itcast/user/config/PatternProperties.java
@@ -0,0 +1,18 @@
+package cn.itcast.user.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description 配置文件
+ * @Author lhw
+ * @Date 2021/9/24 15:41
+ * @Version 1.0
+ **/
+@Component
+@Data
+@ConfigurationProperties(prefix = "pattern")
+public class PatternProperties {
+    public String dateFormat;
+}
--- springCloud/day1/code/cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ springCloud/day1/code/cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,18 +1,31 @@
 package cn.itcast.user.web;
 
-import cn.itcast.user.pojo.User;
+import cn.itcast.user.config.PatternProperties;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
 @RequestMapping("/user")
+//@RefreshScope
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    // 读取nacos配置
+//    @Value("${pattern.dataformat}")
+//    private String dateformat;
+
+    @Autowired
+    private PatternProperties pattern;
 
     /**
      * 路径： /user/110
@@ -20,8 +33,13 @@ public class UserController {
      * @param id 用户id
      * @return 用户
      */
-    @GetMapping("/{id}")
-    public User queryById(@PathVariable("id") Long id) {
-        return userService.queryById(id);
+//    @GetMapping("/{id}")
+//    public User queryById(@PathVariable("id") Long id) {
+//        return userService.queryById(id);
+//    }
+
+    @GetMapping("/now")
+    public String now() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(pattern.getDateFormat()));
     }
 }
